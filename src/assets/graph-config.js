ddc753d4cdb3b51ee5d9729210184c9000b2959e
Wouter Van Rossem <woutervanrossem@gmail.com>
2020-02-06 15:31:38 +0100

    Add expand and collapse for compound nodes plus cose bilkent layout

--- src/assets/graph-config.js
+++ src/assets/graph-config.js
@@ -66,6 +66,59 @@ const colaLayout = {
   infinite: false // overrides all other options for a forces-all-the-time mode
 }
 
+const coseBilkent = {
+  name: 'cose-bilkent',
+  // Called on `layoutready`
+  ready: function () {},
+  // Called on `layoutstop`
+  stop: function () {},
+  // 'draft', 'default' or 'proof" 
+  // - 'draft' fast cooling rate 
+  // - 'default' moderate cooling rate 
+  // - "proof" slow cooling rate
+  quality: 'default',
+  // Whether to include labels in node dimensions. Useful for avoiding label overlap
+  nodeDimensionsIncludeLabels: true,
+  // number of ticks per frame; higher is faster but more jerky
+  refresh: 50,
+  // Whether to fit the network view after when done
+  fit: true,
+  // Padding on fit
+  padding: 10,
+  // Whether to enable incremental mode
+  randomize: true,
+  // Node repulsion (non overlapping) multiplier
+  nodeRepulsion: 4500,
+  // Ideal (intra-graph) edge length
+  idealEdgeLength: 50,
+  // Divisor to compute edge forces
+  edgeElasticity: 0.45,
+  // Nesting factor (multiplier) to compute ideal edge length for inter-graph edges
+  nestingFactor: 0.1,
+  // Gravity force (constant)
+  gravity: 0.25,
+  // Maximum number of iterations to perform
+  numIter: 2500,
+  // Whether to tile disconnected nodes
+  tile: true,
+  // Type of layout animation. The option set is {'during', 'end', false}
+  animate: 'end',
+  // Duration for animate:end
+  animationDuration: 500,
+  // Amount of vertical space to put between degree zero nodes during tiling (can also be a function)
+  tilingPaddingVertical: 10,
+  // Amount of horizontal space to put between degree zero nodes during tiling (can also be a function)
+  tilingPaddingHorizontal: 10,
+  // Gravity range (constant) for compounds
+  gravityRangeCompound: 1.5,
+  // Gravity force (constant) for compounds
+  gravityCompound: 1.0,
+  // Gravity range (constant)
+  gravityRange: 3.8,
+  // Initial cooling factor for incremental layout
+  initialEnergyOnIncremental: 0.5
+}
+
 const config = {
   elements: graph_data,
   style: [
@@ -95,15 +148,46 @@ const config = {
       }
     },
     {
-      selector: 'node:parent',
+      selector: 'node:compound',
       css: {
         'background-opacity': 0.333
       }
     },
+    // {
+    //   selector: 'node:parent[[outdegree < 1]]',
+    //   css: {
+    //     'display': 'none'
+    //   }
+    // },
+    {
+      selector: "node.cy-expand-collapse-collapsed-node",
+      style: {
+        "background-color": "darkblue",
+        "shape": "rectangle"
+      }
+    },
+    {
+      selector: "node[[indegree = 0]][[outdegree = 0]]",
+      style: {
+        'display': "none"
+      }
+    },
+    // {
+    //   selector: "node[[indegree > 5]]",
+    //   style: {
+    //     'display': "none"
+    //   }
+    // },
+    // {
+    //   selector: "node:  ",
+    //   style: {
+    //     'display': "none"
+    //   }
+    // },
   ],
   boxSelectionEnabled: false,
   autounselectify: true,
-  layout: colaLayout,
+  layout: coseBilkent,
 };
 
 export default config;
--- src/components/Graph.vue
+++ src/components/Graph.vue
@@ -20,9 +20,30 @@
 import config from "../assets/graph-config";
 
 import cola from 'cytoscape-cola';
+import coseBilkent from 'cytoscape-cose-bilkent';
+import expandCollapse from 'cytoscape-expand-collapse';
 
 const elements = [...config.elements];
 delete config.elements;
+
+const expandCollapseOptions = {
+  layoutBy: null, // to rearrange after expand/collapse. It's just layout options or whole layout function. Choose your side!
+  // recommended usage: use cose-bilkent layout with randomize: false to preserve mental map upon expand/collapse
+  fisheye: true, // whether to perform fisheye view after expand/collapse you can specify a function too
+  animate: true, // whether to animate on drawing changes you can specify a function too
+  animationDuration: 1000, // when animate is true, the duration in milliseconds of the animation
+  ready: function () {}, // callback when expand/collapse initialized
+  undoable: true, // and if undoRedoExtension exists,
+
+  cueEnabled: true, // Whether cues are enabled
+  expandCollapseCuePosition: 'top-left', // default cue position is top left you can specify a function per node too
+  expandCollapseCueSize: 12, // size of expand-collapse cue
+  expandCollapseCueLineSize: 8, // size of lines used for drawing plus-minus icons
+  expandCueImage: undefined, // image of expand icon if undefined draw regular expand cue
+  collapseCueImage: undefined, // image of collapse icon if undefined draw regular collapse cue
+  expandCollapseCueSensitivity: 1, // sensitivity of expand-collapse cues
+  zIndex: 999 // z-index value of the canvas in which cue ımages are drawn
+}
 
 let resolveCy = null
 export const cyPromise = new Promise(resolve => (resolveCy = resolve))
@@ -56,11 +77,31 @@ export default {
     preConfig(cytoscape) {
       console.log("calling pre-config", cytoscape);
       cytoscape.use(cola);
+      cytoscape.use(coseBilkent);
+      cytoscape.use(expandCollapse);
     },
     async afterCreated(cy) {
       console.log("calling after-created", cy);
       await cy
-      cy.layout(config.layout).run()
+
+      // cy.layout(config.layout).run()
+
+      const api = cy.expandCollapse({
+        layoutBy: {
+          name: "cose-bilkent",
+          animate: "end",
+          randomize: true,
+          fit: true,
+          nodeDimensionsIncludeLabels: true,
+          quality: 'default',
+          tile: false
+        },
+        fisheye: true,
+        animate: true,
+        undoable: false
+      });
+      api.collapseAll();
+
       resolveCy(cy)
     },
   },
