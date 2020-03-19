31f34985aeb0efe429a03b707538cc26eae91b4e
Wouter Van Rossem <wouter@woutervanrossem.eu>
2020-03-19 14:46:43 +0100

    Refactor code to load atlasti xml data

---
+++ scripts/cytoscape_classes.js
@@ -0,0 +1,63 @@
+
+const NODE_TYPES = {
+  DOCUMENT: "document",
+  DOCUMENT_GROUP: "document-group",
+  CODE: "code",
+  CODE_GROUP: "code-group"
+};
+
+const LINK_TYPES = {
+  CODE_DOCUMENT_LINK: "code-document-link",
+  CODE_GROUP_LINK: "code-group-link",
+  CODE_DOCUMENT_GROUP_LINK: "code-document-group-link",
+  DOC_GROUP_LINK: "doc-group-link"
+}
+
+class Node {
+
+  constructor(id, name, classes) {
+    this.id = id;
+    this.name = name;
+    this.classes = classes;
+  }
+
+  toJSON() {
+    return {
+      data: {
+        id: this.id,
+        name: this.name
+      },
+      group: "nodes",
+      classes: this.classes
+    };
+  }
+};
+
+class Edge {
+
+  constructor(id, source, target, classes) {
+    this.id = id;
+    this.source = source;
+    this.target = target;
+    this.classes = classes;
+  }
+
+  toJSON() {
+    return {
+      data: {
+        id: this.id,
+        source: this.source,
+        target: this.target
+      },
+      group: "edges",
+      classes: this.classes
+    }
+  }
+};
+
+module.exports = {
+  Node,
+  Edge,
+  NODE_TYPES,
+  LINK_TYPES
+}
