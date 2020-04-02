b37379de506afe9fda438580f2b65df5c7dcfb6a
Wouter Van Rossem <wouter@woutervanrossem.eu>
2020-04-02 17:53:21 +0200

    Add set operations and node table

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,87 @@
 <template>
   <div id="app" class="level">
     <div class="container">
+
+      <div class="level">
+        <div class="level-left"> 
+          <div class="level-item">
+            <b-field label="Select first ontology">
+              <b-select placeholder="Select" v-model="selectedSet1">
+                <option value="df_2">DE: Xauslander</option>
+                <option value="df_5">EU: Eurodac</option>
+                <option value="df_6">EU: SIS</option>
+                <option value="df_7">EU: VIS</option>
+                <option value="df_9">GR: XKA</option>
+                <option value="df_12">IO: IOM - MiMOSA</option>
+              </b-select>
+            </b-field>
+          </div>
+          <div class="level-item">
+            <b-field label="Set operation">
+              <b-select placeholder="Select" v-model="selectedOperation">
+                <!-- <option value="union">Union</option> -->
+                <option value="intersection">Intersection</option>
+                <option value="difference">Difference</option>
+              </b-select>
+            </b-field>
+          </div>
+          <div class="level-item">
+            <b-field label="Select second ontology">
+              <b-select placeholder="Select" v-model="selectedSet2">
+                <option value="df_2">DE: Xauslander</option>
+                <option value="df_5">EU: Eurodac</option>
+                <option value="df_6">EU: SIS</option>
+                <option value="df_7">EU: VIS</option>
+                <option value="df_9">GR: XKA</option>
+                <option value="df_12">IO: IOM - MiMOSA</option>
+              </b-select>
+            </b-field>
+          </div>
+
+          <div class="level-item">
+            <b-field label="Show category groups">
+              <b-switch v-model="showCategoryGroups"></b-switch>
+            </b-field>
+          </div>
+
+          <div class="level-item">
+            <b-field label="✨✨">
+              <button class="button"
+                v-on:click="startSetOperation()">
+                Submit
+              </button>
+            </b-field>
+          </div>
+
+          
+        </div>
+      </div>
+
+      <div class="level">
+        <div class="level-left"> 
+          <div class="level-item">
+            
+          </div>
+        </div>
+      </div>
+
       <div class="columns">
-        <div class="column is-two-thirds">
-          <Graph
-            v-on:set-selected-node="onSetSelectedNode"
-          ></Graph>
+        <div class="column is-one-thirds">
+          <NodeTable v-bind:data="nodes"></NodeTable>
         </div>
         <div class="column">
-          <NodeInfo
-            v-bind:node="selectedNode"
-          />
+          
+          <Graph
+            ref="graph"
+            v-on:set-selected-node="onSetSelectedNode"
+            v-on:set-operation-results="onSetOperationResult"
+          ></Graph>
+
+          <hr>
+
+          <!-- <NodeInfo -->
+            <!-- v-bind:node="selectedNode" -->
+          <!-- /> -->
         </div>
       </div>
     </div>
@@ -21,16 +92,24 @@
 import Graph from './components/Graph.vue'
 import NodeInfo from './components/NodeInfo.vue'
 import clone from 'lodash/clone';
+import head from 'lodash/head';
+import NodeTable from './components/NodeTable.vue'
 
 export default {
   name: 'app',
   components: {
     Graph,
-    NodeInfo
+    // NodeInfo,
+    NodeTable
   },
   data() {
     return {
-      selectedNode: this.initialSelectedNode
+      selectedNode: this.initialSelectedNode,
+      selectedSet1: "",
+      selectedSet2: "",
+      selectedOperation: "",
+      showCategoryGroups: false,
+      nodes: []
     }
   },
   props: {
@@ -46,14 +125,31 @@ export default {
   },
   methods: {
     onSetSelectedNode: function (node) {
-      // let nodeClone = clone(node);
-      // let neighborhood = node.neighborhood.filter((el) => el.group === "nodes").map((el) => el.data.name);
       let nodeClone = {
         "name": node.name,
         "neighborhood": node.neighborhood
       };
-      console.log("test", nodeClone)
       this.selectedNode = nodeClone;
+    },
+    startSetOperation: function () {
+      console.log("starting set operation from app")
+      this.$refs.graph.startSetOperation(this.selectedSet1, this.selectedSet2, this.selectedOperation);
+    },
+    onSetOperationResult: function (nodes) {
+      let resultClone = nodes;
+
+      if(!this.showCategoryGroups) {
+        resultClone = resultClone.filter((node) => head(node.classes) == "code");
+      }
+
+      resultClone = resultClone.map((node) => {
+        return {
+          "id": node.id,
+          "node_name": node.name,
+          "node_type": head(node.classes)
+        }
+      });
+      this.nodes = resultClone;
     }
   }
 }
