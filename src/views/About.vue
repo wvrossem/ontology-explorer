98689cd905cb084abcd1f7d6bcc0b28e2091abd6
Wouter Van Rossem <wouter@woutervanrossem.eu>
2020-06-29 18:19:48 +0200

    Add content and styling to the about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,72 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="section" id="about-text">
+    <div class="container">
+      <div class="column is-10 is-offset-1">
+        <h1 class="title">About Processing Citizenship</h1>
+
+        <div class="content">
+          <p>
+            How does migration enact Europe? This question can be answered legally and politically, as most policy makers, sociologists and journalists do. Or, it can be answered
+            <strong>technically.</strong> How do data infrastructures and practices for foreign population management shape the European order, while they process Alterity?
+          </p>
+          <p>
+            Current migration waves are changing not only European policies, but also the way
+            <strong>knowledge</strong> about individuals, institutions and space is produced.
+            <strong>Interoperable data systems</strong> are key enablers of this knowledge. They crystallize security, humanitarian, administrative and technical dynamics that compete to define what “alterity”, “citizenship”, “state” and “Europe” are. This is the main insight of
+            <i>Processing Citizenship</i>
+            <i>. Digital registration of migrants as co-production of citizens, territory and Europe</i>, a five-year research program involving a team made of&nbsp; sociologists of technology, information scientists, ethnographers and political sociologists.
+          </p>
+          <p>
+            Thanks to the support of an
+            <a
+              href="https://erc.europa.eu/"
+              target="_blank"
+              rel="noopener"
+            >
+              <strong>ERC Starting Grant</strong>
+            </a> (2017-22), we are studying registration and identification of third-country nationals in Europe as socio-technical practices. Such practices challenge our established notions of “alterity”, “citizenship”, “state”, “Europe” and “territory”. This evidence raises pressing technical and crucial long-term issues.
+          </p>
+          <p>
+            Technically, migrant data circulation requires infrastructural standardization and integration among agencies at European, national and local levels.
+            <strong>Gaps and misalignments</strong> in data collection, classification and circulation can lead to major drawbacks not only in the European migration regime, but also in European multi-level governance. Processing Citizenship (PC) studies such gaps and misalignments from the perspective of
+            <strong>semantic interoperability</strong> and
+            <strong>data quality</strong>.
+          </p>
+          <p>
+            At the same time,
+            <strong>Processing Citizenship</strong> aims to develop a “history of the present” that accounts for contemporary material practices of registration and identification of Alterity as activities of long-term governance transformation. As historians of technology have shown, modern institutions like the nation-state are the
+            <strong>result</strong>, not the cause, of efforts to handle information about populations and territory. The nation-state can be conceived of as the most powerful socio-technical machine for knowledge handling. More recently, European integration has been shown to be the result of multi-level and multi-sector activities of infrastructure building. As a consequence, we wonder which
+            <strong>new orders of authority</strong> are emerging from contemporary European data infrastructures and practices for population management. In other words, Processing Citizenship investigates the infrastructural
+            <strong>“re-making” of Europe</strong>, one of the most fundamental since the Treaty of Rome 60 years ago.
+          </p>
+          <p>
+            <a
+              href="http://processingcitizenship.eu/project/pcs-objectives/"
+              target="_blank"
+              rel="noopener"
+            >
+              <em>Read more about PC's objectives</em>
+            </a>
+          </p>
+          <p>
+            <a
+              href="http://processingcitizenship.eu/project/script-based-methodology/"
+              target="_blank"
+              rel="noopener"
+            >
+              <em>Read more about PC's script methodology</em>
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+
+<style>
+#about-text {
+  text-align: left;
+  font-family: "Arvo", sans-serif;
+  /* font-size: 16px; */
+}
+</style>
