637c802b889434109c994ded0b59713f3648904a
Wouter Van Rossem <wouter@woutervanrossem.eu>
2020-06-29 18:19:18 +0200

    Cleanup the header and add proper navigation bar

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,56 @@
 <template>
   <div id="app">
+    <!-- <section class="hero">
+      <div class="hero-body">
+        <div class="container">
+          <div class="columns">
+            <div class="column is-narrow">
+              <figure class="image is-128x128">
+                <img src="logoPC.jpg" />
+              </figure>
+            </div>
+            <div class="column">
+              <h1 class="title">Ontology explorer</h1>
+              <h2 class="subtitle">Experiment combining Vue.js and cytoscape for network analysis</h2>
+
+              <div id="nav">
+            <b-navbar>
+              <template slot="start">
+                <router-link class="navbar-item" to="/">Home</router-link>|
+                <router-link class="navbar-item" to="/upload">Upload</router-link>|
+                <router-link class="navbar-item" to="/about">About</router-link>
+              </template>
+            </b-navbar>
+          </div>
+            </div>
+          </div>
+
+          
+        </div>
+      </div>
+    </section> -->
+
     <div id="nav">
-      <router-link to="/">Home</router-link>|
-      <router-link to="/about">About</router-link>
+      <b-navbar 
+        spaced
+        shadow>
+        <template slot="brand">
+          <b-navbar-item tag="router-link" :to="{ path: '/' }">
+            <img
+              src="logoPC.jpg"
+              alt="Processing Citizenship"
+            />
+          </b-navbar-item>
+        </template>
+        <hr class="navbar-divider">
+        <template slot="start">
+          <router-link class="navbar-item" to="/">Home</router-link>
+          <hr class="navbar-divider">
+          <router-link class="navbar-item" to="/upload">Upload</router-link>
+          <hr class="navbar-divider">
+          <router-link class="navbar-item" to="/about">About</router-link>
+        </template>
+      </b-navbar>
     </div>
     <router-view />
   </div>
@@ -17,33 +65,39 @@ $title-color: #bf0000;
 
 $hero-body-padding: 1rem 1rem;
 
+$navbar-item-img-max-height: 4rem;
+
 // Import Bulma and Buefy styles
 @import "~bulma";
 @import "~buefy/src/scss/buefy";
 
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
 
 #nav {
-  padding: 30px;
-
   a {
     font-weight: bold;
     color: #2c3e50;
 
     &.router-link-exact-active {
-      color: #42b983;
+      color: #bf0000;
     }
   }
 }
 
 h1 {
   color: #bf0000;
+}
+
+a {
+  color: #bf0000;
+}
+
+strong {
+  color: #2c3e50;
 }
 
 #cytoscape-div {
