b1b38a84cb0687269871833b8227aba6923d92e4
Wouter Van Rossem <wouter@woutervanrossem.eu>
2020-07-07 22:18:02 +0200

    Cleanup code of Graph component

--- src/components/Graph.vue
+++ src/components/Graph.vue
@@ -17,7 +17,6 @@
 </template>
 
 <script>
-// import config from "../assets/graph-config";
 import { mapState, mapGetters } from "vuex";
 import get from 'lodash/get';
 import head from 'lodash/head';
@@ -25,11 +24,8 @@ import join from 'lodash/join';
 
 import cola from "cytoscape-cola";
 import coseBilkent from "cytoscape-cose-bilkent";
-import expandCollapse from "cytoscape-expand-collapse";
 import klay from "cytoscape-klay";
 import cise from "cytoscape-cise";
-
-let prevSelected = "";
 
 let resolveCy = null;
 export const cyPromise = new Promise(resolve => (resolveCy = resolve));
@@ -37,7 +33,6 @@ export const cyPromise = new Promise(resolve => (resolveCy = resolve));
 export default {
   data() {
     return {
-      // config,
       prevSelected
     };
   },
@@ -49,56 +44,16 @@ export default {
   },
   props: {},
   methods: {
-    /* eslint-disable no-console */
-    addNode(event) {
-      // console.log(event.target, this.$refs.cyRef.instance);
-      // if (event.target === this.$refs.cyRef.instance)
-      //   console.log("adding node", event.target);
-    },
-    clickNode(event, data) {
-      // let name = event.target.data().name;
-      // let node = event.target.data();
-
-      // const cy = this.$refs.cyRef.instance;
-
-      // if (this.prevSelected.length > 0) {
-      //   cy.$(this.prevSelected).toggleClass("selected");
-      // }
-
-      // let neighborhood = cy.$(`#${node.id}`).closedNeighborhood();
-
-      // let ids = neighborhood.map(el => `#${el.id()}`).join(', ');
-      // console.log(ids);
-      // cy.$(ids).toggleClass("selected");
-
-      // this.prevSelected = ids;
-
-      // neighborhood = neighborhood.map(el => {
-      //   return {
-      //     name: el.data().name,
-      //     classes: el.classes(),
-      //     group: el.group()
-      //   };
-      // });
-      // neighborhood = neighborhood.filter(el => el.group === "nodes");
-
-      // node.neighborhood = neighborhood;
-
-      // this.$emit("set-selected-node", node);
-    },
+    addNode(event) {},
+    clickNode(event, data) {},
     updateNode(event) {},
-    showNodes(nodes) {},
-    searchNode(node) {},
     preConfig(cytoscape) {
-      console.log("calling pre-config", cytoscape);
       cytoscape.use(cola);
       cytoscape.use(coseBilkent);
-      // cytoscape.use(expandCollapse);
       cytoscape.use(klay);
       cytoscape.use(cise);
     },
     async afterCreated(cy) {
-      console.log("calling after-created", cy);
       await cy;
 
       cy.layout(this.config.layout).run();
